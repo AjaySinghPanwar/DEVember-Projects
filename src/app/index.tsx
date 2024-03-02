84e41e2e592111c1d04bf758c2cbf72179deb916
Ajay Singh Panwar <a.panwar48656@gmail.com>
2024-03-02 19:58:15 +0530

    Move fonts loading in root layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,36 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { FlatList, StyleSheet, View } from "react-native";
-import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
-import {
-  AmaticSC_400Regular,
-  AmaticSC_700Bold,
-} from "@expo-google-fonts/amatic-sc";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
 import DayListItem from "../components/DayListItem";
-
-// Keep the splash screen visible while we fetch resources
-SplashScreen.preventAutoHideAsync();
 
 const days = [...Array(24)].map((_value, index) => index + 1);
 
 export default function HomeScreen() {
-  const [fontsLoaded, fontError] = useFonts({
-    Inter: Inter_900Black,
-    Amatic: AmaticSC_400Regular,
-    AmaticBold: AmaticSC_700Bold,
-  });
-
-  useEffect(() => {
-    if (fontsLoaded || fontError) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontError]);
-
-  if (!fontsLoaded && !fontError) {
-    return;
-  }
-
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
